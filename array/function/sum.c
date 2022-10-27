dac757c7057932aff98996fb0d5342ad0d91b9c3
ashik <ashiashik5047@gmail.com>
2022-10-27 21:50:46 +0530

    function

---
+++ array/function/sum.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int sum(){
+    int a[100],i,n,sum=0;
+    printf("enter the limit:\n");
+    scanf("%d",&n);
+    printf("|enter the values:\n");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+        sum =sum+a[i];
+    }
+    printf("sum is %d",sum);
+}
+void main()
+{
+    sum();
+}
